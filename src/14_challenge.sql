73bd68f16c26e20083d8d83556944c9cec1e7ff3
Izadora Braga <izadora_abraga@outlook.com>
2024-02-23 21:46:49 -0300

    requisito 14

---
+++ src/14_challenge.sql
@@ -0,0 +1,13 @@
+SELECT 
+    artists.name AS 'Artista', 
+    GROUP_CONCAT(albums.title) AS 'Álbuns'
+FROM 
+    artists
+JOIN 
+    albums 
+ON 
+    artists.id = albums.artist_id
+GROUP BY 
+    artists.name
+ORDER BY
+    artists.name;
